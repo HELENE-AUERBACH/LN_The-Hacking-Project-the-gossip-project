e268e7c508d883eb35b98344320ed91eeb953232
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-05-10 21:20:01 +0200

    Ajout de la page de bienvenue qui affiche le prénom que l'utilisateur a saisi dans l'URL.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/:first_name', to: 'welcome#show'
 end
